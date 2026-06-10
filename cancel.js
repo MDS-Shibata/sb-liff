b1b2e10b1e76d008fc73560289dc317c4a6701d1
MDS-Shibata <internetmailaddress1@gmail.com>
2026-06-10 11:30:38 +0900

    Update cancel.js

--- cancel.js
+++ cancel.js
@@ -119,9 +119,7 @@ reservations.forEach((r) => {
       return;
     }
 
-    // ★ キャンセル開始 → ローディング表示
-  document.getElementById("loading").style.display = "block";
-
+   
     const targets = [];
     checks.forEach(c => {
       targets.push({
