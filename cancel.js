4339210d4299552d869bb3106bc9a9b713d9b1ac
MDS-Shibata <internetmailaddress1@gmail.com>
2026-06-10 09:29:36 +0900

    Update cancel.js

--- cancel.js
+++ cancel.js
@@ -46,8 +46,8 @@ console.log("GASから届いたデータ:", reservations);
   // 表の枠を作成
 const list = document.getElementById("list");
 list.innerHTML = `
-  <div style="margin-bottom: 40px;font-size: 35px;">
-    <h3 style="background:#7fffd4; padding:50px;">登校予約</h3>
+  <div style="margin-bottom: 40px;font-size: 50px;">
+    <h3 style="background:#7fffd4; padding:30px;">登校予約</h3>
     <table border="1" style="width:100%; border-collapse:collapse;">
       <thead>
         <tr style="background:#f8f8f8;">
@@ -58,8 +58,8 @@ list.innerHTML = `
     </table>
   </div>
 
-  <div style="margin-bottom: 40px;font-size: 35px;">
-    <h3 style="background:#e0f0ff; padding:50px;">下校予約</h3>
+  <div style="margin-bottom: 40px;font-size: 50px;">
+    <h3 style="background:#e0f0ff; padding:30px;">下校予約</h3>
     <table border="1" style="width:100%; border-collapse:collapse;">
       <thead>
         <tr style="background:#f8f8f8;">
